2d31cc7840676ec01ab2731dbf273daba8b2a3d3
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-06-15 16:51:15 +0200

    conditional queries

--- CleanArchitecture.ConsoleApp/Program.cs
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -1,10 +1,36 @@
 ﻿using CleanArchitecture.Data;
 using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
 
 StreamerDbContext dbContext = new();
 
+await QueryFilter();
+//QueryStreaming();
 //await AddNewRecords();
-QueryStreaming();
+
+async Task QueryFilter()
+{
+    Console.WriteLine("Introduce el nombre de la compañia: ");
+    var companyName = Console.ReadLine();
+    //var streamers = await dbContext.Streamers!.Where(s => s.Name == companyName).ToListAsync();
+    // La misma consulta pero con la sintaxis de comparacion en c#
+    //var streamers = await dbContext.Streamers!.Where(s => s.Name.Equals(companyName)).ToListAsync();
+
+    // Utiliza el método Contains de C#, que se traduce en una cláusula LIKE en SQL
+    //var streamers = await dbContext.Streamers!.Where(s => s.Name.Contains(companyName!)).ToListAsync();
+
+    // Utiliza directamente la función LIKE de SQL a través de EF.Functions.Like
+    var streamers = await dbContext.Streamers!.Where(s => EF.Functions.Like(s.Name, $"%{companyName}%")).ToListAsync();
+
+
+    if (streamers.Any())
+    {
+        Console.WriteLine($"{streamers.First().Name} - {streamers.First().Url}");
+        return;
+    }
+    Console.WriteLine("No existe la compañia");
+}
+
 void QueryStreaming()
 {
     var streamers = dbContext!.Streamers!.ToList();
