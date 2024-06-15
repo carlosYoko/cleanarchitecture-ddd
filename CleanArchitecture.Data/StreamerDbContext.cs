f9940e4bd3c9d864e920a0a04ff2814d14a3b61d
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-06-15 12:36:44 +0200

    create connection string

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=127.0.0.1,14333;Database=Streamer;User Id=sa;Password=PasswordO1.");
+        }
+
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+    }
+}
