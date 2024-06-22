65e31926948b385914606652d94ac409d95803b9
Carlos Gimenez <carlosgibe@gmail.com>
2024-06-22 18:58:13 +0200

    dependency injections

---
+++ CleanArchitecture.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.Behaviors;
+using CleanArchitecture.Application.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArchitecture.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
