5109f0c0e57a3c66a1cde0c0302ae0fa1228a881
Carlos Gimenez <carlosgibe@gmail.com>
2024-06-22 17:46:50 +0200

    delete command

---
+++ CleanArchitecture.Application/Features/Streamers/Commands/DeleteStreamer/DeleteStreamerCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands.DeleteStreamer
+{
+    public class DeleteStreamerCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ CleanArchitecture.Application/Features/Streamers/Commands/DeleteStreamer/DeleteStreamerCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Exceptions;
+using CleanArchitecture.Domain;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands.DeleteStreamer
+{
+    public class DeleteStreamerCommandHandler : IRequestHandler<DeleteStreamerCommand>
+    {
+        private readonly IStreamerRepository? _streamerRepository;
+        private readonly IMapper? _mapper;
+        private readonly ILogger<DeleteStreamerCommandHandler>? _logger;
+
+        public DeleteStreamerCommandHandler(IStreamerRepository? streamerRepository, IMapper? mapper, ILogger<DeleteStreamerCommandHandler>? logger)
+        {
+            this._streamerRepository = streamerRepository;
+            this._mapper = mapper;
+            this._logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteStreamerCommand request, CancellationToken cancellationToken)
+        {
+            var streamerToDelete = await _streamerRepository!.GetByIdAsync(request.Id);
+            if (streamerToDelete == null)
+            {
+                _logger!.LogError($"El streamer {request.Id} no exite");
+                throw new NotFoundExcpetion(nameof(Streamer), request.Id);
+            }
+
+            await _streamerRepository.DeleteAsync(streamerToDelete);
+            _logger!.LogInformation($"El streamer {request.Id} ha sido eliminado con exito");
+
+            return Unit.Value;
+        }
+    }
+}
