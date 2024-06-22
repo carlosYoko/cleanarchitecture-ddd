37b920f53abccc28a8311ad1a3bdca5ea380d8a4
Carlos Gimenez <carlosgibe@gmail.com>
2024-06-22 19:35:10 +0200

    create controller methods

---
+++ CleanArchitecture.API/Controllers/StreamerController.cs
@@ -0,0 +1,51 @@
+﻿using CleanArchitecture.Application.Features.Streamers.Commands.CreateStreamer;
+using CleanArchitecture.Application.Features.Streamers.Commands.DeleteStreamer;
+using CleanArchitecture.Application.Features.Streamers.Commands.UpdateStreamer;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class StreamerController : ControllerBase
+    {
+        private readonly IMediator? _mediator;
+
+        public StreamerController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpPost(Name = "CreateStreamer")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> CreateStreamer([FromBody] CreateStreamerCommand command)
+        {
+            return await _mediator!.Send(command);
+        }
+
+        [HttpPut(Name = "UpdateStreamer")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateStreamer([FromBody] UpdateStreamerCommand command)
+        {
+            await _mediator!.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteStreamer")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> DeleteStreamer(int id)
+        {
+            var command = new DeleteStreamerCommand() { Id = id };
+
+            await _mediator!.Send(command);
+
+            return NoContent();
+        }
+    }
+}
