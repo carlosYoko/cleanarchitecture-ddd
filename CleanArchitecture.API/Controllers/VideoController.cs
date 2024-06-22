6be594add8839068660cc67a4dd988f0b59b63cb
Carlos Gimenez <carlosgibe@gmail.com>
2024-06-22 19:20:32 +0200

    presentation layer

---
+++ CleanArchitecture.API/Controllers/VideoController.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchitecture.Application.Features.Videos.Queries.GetVideosList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class VideoController : ControllerBase
+    {
+        private readonly IMediator? _mediator;
+
+        public VideoController(IMediator? mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpGet("{userName}", Name = "GetVideo")]
+        [ProducesResponseType(typeof(IEnumerable<VideosVM>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<VideosVM>>> GetVideosByUserName(string username)
+        {
+            var query = new GetVideosListQuery(username);
+            var videos = await _mediator!.Send(query);
+
+            return Ok(videos);
+        }
+    }
+
+}
