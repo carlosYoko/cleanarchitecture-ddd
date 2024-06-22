3b93252c52a2013d1c93dd94153f79060033f5c1
Carlos Gimenez <carlosgibe@gmail.com>
2024-06-22 13:52:55 +0200

    query command and handlers

--- CleanArchitecture.Application/Contracts/Persistence/IVideoRepository.cs
+++ CleanArchitecture.Application/Contracts/Persistence/IVideoRepository.cs
@@ -4,6 +4,7 @@ namespace CleanArchitecture.Application.Contracts.Persistence
 {
     public interface IVideoRepository : IAsyncRepository<Video>
     {
-        Task<IEnumerable<Video>> GetVideoByName(string name);
+        Task<Video> GetVideoByName(string videoName);
+        Task<IEnumerable<Video>> GetVideoByUserName(string userName);
     }
 }
---
+++ CleanArchitecture.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Features.Videos.Queries.GetVideosList;
+using CleanArchitecture.Domain;
+
+namespace CleanArchitecture.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Video, VideosVM>();
+        }
+    }
+}
