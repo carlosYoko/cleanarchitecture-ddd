9fa5f54c65901ac2ebf57fe41add118f3ff7799f
Carlos Gimenez <carlosgibe@gmail.com>
2024-06-23 12:47:50 +0200

    generic methods

--- CleanArchitecture.Application/Contracts/Persistence/IAsyncRepository.cs
+++ CleanArchitecture.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -16,9 +16,9 @@ namespace CleanArchitecture.Application.Contracts.Persistence
                                         string? includeString = null,
                                         List<Expression<Func<T, object>>>? includes = null,
                                         bool disableTracking = true);
-        Task<T> GetByIdAsync(int id);
+        Task<T?> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
